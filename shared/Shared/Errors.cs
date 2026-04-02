70e6236be4cceddfaf3640e06dbfc48217334b7a
Petlevannaya-Olga <olga.g.afonina@gmail.com>
2026-04-03 00:41:34 +0300

    Shared

---
+++ shared/Shared/Errors.cs
@@ -0,0 +1,21 @@
+namespace Shared;
+
+using System.Collections;
+
+public class Errors(IEnumerable<Error> errors) : IEnumerable<Error>
+{
+    private readonly List<Error> _errors = [..errors];
+
+    public IEnumerator<Error> GetEnumerator()
+    {
+        return _errors.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+    public static implicit operator Errors(Error[] errors)
+        => new(errors);
+
+    public static implicit operator Errors(Error error)
+        => new([error]);
+}
