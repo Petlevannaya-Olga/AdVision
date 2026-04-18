c430398646eb7571acdb669820190059ce36a9d6
Petlevannaya-Olga <olga.g.afonina@gmail.com>
2026-04-18 16:23:51 +0300

    fix paging control

--- src/AdVision.Presentation/PagingUserControl.Designer.cs
+++ src/AdVision.Presentation/PagingUserControl.Designer.cs
@@ -28,30 +28,43 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			lblPaging = new Label();
+			tableLayoutPanel1 = new TableLayoutPanel();
 			btnPrev = new Button();
 			btnNext = new Button();
 			btnAdd = new Button();
+			lblPaging = new Label();
+			tableLayoutPanel1.SuspendLayout();
 			SuspendLayout();
 			// 
-			// lblPaging
+			// tableLayoutPanel1
 			// 
-			lblPaging.AutoSize = true;
-			lblPaging.Location = new Point(3, 9);
-			lblPaging.Name = "lblPaging";
-			lblPaging.Size = new Size(58, 20);
-			lblPaging.TabIndex = 6;
-			lblPaging.Text = "label11";
+			tableLayoutPanel1.ColumnCount = 5;
+			tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+			tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 50F));
+			tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 50F));
+			tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 50F));
+			tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 150F));
+			tableLayoutPanel1.Controls.Add(lblPaging, 0, 0);
+			tableLayoutPanel1.Controls.Add(btnNext, 3, 0);
+			tableLayoutPanel1.Controls.Add(btnPrev, 1, 0);
+			tableLayoutPanel1.Controls.Add(btnAdd, 4, 0);
+			tableLayoutPanel1.Dock = DockStyle.Fill;
+			tableLayoutPanel1.Location = new Point(0, 0);
+			tableLayoutPanel1.Name = "tableLayoutPanel1";
+			tableLayoutPanel1.RowCount = 1;
+			tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+			tableLayoutPanel1.Size = new Size(754, 56);
+			tableLayoutPanel1.TabIndex = 0;
 			// 
 			// btnPrev
 			// 
 			btnPrev.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
 			btnPrev.BackColor = Color.Transparent;
 			btnPrev.FlatStyle = FlatStyle.Popup;
-			btnPrev.Location = new Point(276, 5);
+			btnPrev.Location = new Point(457, 24);
 			btnPrev.Name = "btnPrev";
 			btnPrev.Size = new Size(44, 29);
-			btnPrev.TabIndex = 10;
+			btnPrev.TabIndex = 14;
 			btnPrev.Text = "<";
 			btnPrev.UseVisualStyleBackColor = false;
 			// 
@@ -60,10 +73,10 @@
 			btnNext.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
 			btnNext.BackColor = Color.Transparent;
 			btnNext.FlatStyle = FlatStyle.Popup;
-			btnNext.Location = new Point(326, 5);
+			btnNext.Location = new Point(557, 24);
 			btnNext.Name = "btnNext";
 			btnNext.Size = new Size(44, 29);
-			btnNext.TabIndex = 11;
+			btnNext.TabIndex = 15;
 			btnNext.Text = ">";
 			btnNext.UseVisualStyleBackColor = false;
 			// 
@@ -72,32 +85,40 @@
 			btnAdd.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
 			btnAdd.BackColor = Color.Transparent;
 			btnAdd.FlatStyle = FlatStyle.Popup;
-			btnAdd.Location = new Point(376, 5);
+			btnAdd.Location = new Point(657, 24);
 			btnAdd.Name = "btnAdd";
 			btnAdd.Size = new Size(94, 29);
-			btnAdd.TabIndex = 12;
+			btnAdd.TabIndex = 16;
 			btnAdd.Text = "Добавить";
 			btnAdd.UseVisualStyleBackColor = false;
+			// 
+			// lblPaging
+			// 
+			lblPaging.AutoSize = true;
+			lblPaging.Location = new Point(3, 0);
+			lblPaging.Name = "lblPaging";
+			lblPaging.Size = new Size(58, 20);
+			lblPaging.TabIndex = 13;
+			lblPaging.Text = "label11";
 			// 
 			// PagingUserControl
 			// 
 			AutoScaleDimensions = new SizeF(8F, 20F);
 			AutoScaleMode = AutoScaleMode.Font;
-			Controls.Add(btnPrev);
-			Controls.Add(btnNext);
-			Controls.Add(btnAdd);
-			Controls.Add(lblPaging);
+			Controls.Add(tableLayoutPanel1);
 			Name = "PagingUserControl";
-			Size = new Size(483, 40);
+			Size = new Size(754, 56);
+			tableLayoutPanel1.ResumeLayout(false);
+			tableLayoutPanel1.PerformLayout();
 			ResumeLayout(false);
-			PerformLayout();
 		}
 
 		#endregion
 
+		private TableLayoutPanel tableLayoutPanel1;
 		private Label lblPaging;
-		private Button btnPrev;
 		private Button btnNext;
+		private Button btnPrev;
 		private Button btnAdd;
 	}
 }
